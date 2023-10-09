1c733caeeb64ac2b40700125e928d2273df68c1c
Nora Clarke <clarke582@gmail.com>
2023-10-09 12:42:05 -0400

    Added a new folder titled SideFiles for files that are not direct python code. Moved wordlist here. Changed hangman accordingly.

--- hangman.py
+++ hangman.py
@@ -62,7 +62,7 @@ play = True
 words = []
 alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-fopen = open('C:\\Users\\clark\\Desktop\\Python\\wordlist.txt', "r")
+fopen = open('SideFiles/wordlist.txt', "r")
 for line in fopen:
     words.append(line.strip())
 
--- test.py
+++
@@ -1,5 +0,0 @@
-board_spots = [board[0][0], board[0][2], board[0][4], board[2][0], board[2][2], board[2][4], board[4][0], board[4][2], board[4][4]]
-
-for spot in board_spots:
-    
-    
